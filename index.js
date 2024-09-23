7acdde05b5a7a8000d60f3faac6bf2660e3e087b
TuuKeZu <moilanentuukka5@gmail.com>
2024-09-23 13:55:45 +0300

    implement cd

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 
 let text = {};
+let logs = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     const LOG = document.getElementById('LOG');
+    const DOWNLOAD = document.getElementById('DOWNLOAD');
     const r = document.getElementById('r');
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get('name') ?? 'unknown';
+    document.getElementById('NAME').textContent = name;
+
     Array.from(document.getElementsByClassName('b')).forEach(btn => {
         const id = btn.getAttribute('target');
         const target = document.getElementById(id);
 
+        if (urlParams.has(id)) {
+            target.parentNode.classList.add('disabled');
+            target.textContent = 'Pois käytöstä';
+            return;
+        }
+
         text[id] = {
             element: target,
             occupied: false,
+            disabled: false,
             start: new Date(),
             value: 0
         };
@@ -21,7 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
             
             if (text[id].occupied) {
                 const t = document.createElement('p');
-                t.textContent = `${id}: ${text[id].start.toLocaleTimeString('FI-fi')} (${fmt(text[id].value)})`;
+                
+                const data = `${id}: ${text[id].start.toLocaleTimeString('FI-fi')} - ${(new Date()).toLocaleTimeString('FI-fi')} (${fmt(text[id].value)})`;
+                const rawData = `${id}/${text[id].start.getTime()}/${(new Date()).getTime()}/${text[id].value}`;
+                t.textContent = data;
+                logs.push(rawData);
                 LOG.appendChild(t);
             } else {
                 text[id].start = new Date();
@@ -30,6 +47,10 @@ document.addEventListener('DOMContentLoaded', () => {
             text[id].value = 0;
             text[id].occupied = !text[id].occupied;
         });
+    });
+
+    DOWNLOAD.addEventListener('click', () => {
+        download(`${name}-logs.txt`, logs.join('\n'));
     });
 
     const fmt = (v) => {
@@ -45,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
         Object.keys(text).forEach(k => {
             let v = text[k];
             v.value += 1;
-            if (!v.occupied && v.value > min) {
+            if (!v.occupied && !v.disabled && v.value > min) {
                 min = v.value;
                 tmp = k;
             }
